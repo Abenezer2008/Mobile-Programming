e7cd0463088eeed1c0c7b30bc8fef610927508d3
abenezer2008 <abenezersintayehu22@gmail.com>
2022-02-27 21:10:02 -0600

    done assignment-3-week-4

---
+++ app/assignment-3-lesson-4-part1/src/main/java/com/example/assignment_3_lesson_4/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.assignment_3_lesson_4
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.AppBarConfiguration
+import android.widget.TableRow
+import android.widget.TextView
+import com.example.assignment_3_lesson_4.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        addRow("Android 3.0", "Honeycomb")
+        addRow("Android 4.0", "Ice Cream Sandwich")
+
+        binding.btnAdd.setOnClickListener {
+            addRow(
+                binding.etAndroidVersion.text.toString().trim(),
+                binding.etAndroidCodeNm.text.toString().trim()
+            )
+        }
+    }
+
+    @SuppressLint("ResourceType")
+    private fun addRow(versionName: String, codeName: String) {
+        val tableRow = TableRow(this)
+
+        val androidVersion = TextView(this)
+        androidVersion.text = versionName
+
+        val androidName = TextView(this)
+        androidName.text = codeName
+
+        tableRow.addView(androidVersion, 0)
+        tableRow.addView(androidName, 1)
+
+        val tvVersionParam = TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT).apply {
+            setMargins(0, 5, 8, 0)
+        }
+        tvVersionParam.weight = 1f
+        androidVersion.setBackgroundResource(R.color.table_bg)
+        androidVersion.layoutParams = tvVersionParam
+
+        val tvCodeNameParam = TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT).apply {
+            setMargins(0, 2, 0, 0)
+        }
+        tvCodeNameParam.weight = 1f
+        androidName.setBackgroundResource(R.color.table_bg)
+        androidName.layoutParams = tvCodeNameParam
+
+        binding.androidTable.addView(tableRow)
+    }
+}
